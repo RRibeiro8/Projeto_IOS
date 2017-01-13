d9cd00fed8be8602b6fa6c9f94e535b99da32b6c
RRibeiro8 <rfribeiro@ua.pt>
2017-01-13 18:19:30 +0000

    UI ViewConnections

---
+++ Projeto_IOS/ConnectionsViewController.h
@@ -0,0 +1,22 @@
+//
+//  ConnectionsViewController.h
+//  Projeto_IOS
+//
+//  Created by RRibeiro on 13/01/17.
+//  Copyright © 2017 RRibeiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConnectionsViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *txtName;
+@property (weak, nonatomic) IBOutlet UISwitch *swVisible;
+@property (weak, nonatomic) IBOutlet UITableView *tblConnectedDevices;
+@property (weak, nonatomic) IBOutlet UIButton *btnDisconnect;
+
+- (IBAction)browseForDevices:(id)sender;
+- (IBAction)toggleVisibility:(id)sender;
+- (IBAction)disconnect:(id)sender;
+
+@end

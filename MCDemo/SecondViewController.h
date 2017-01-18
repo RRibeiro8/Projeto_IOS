b3c6a0f4e4e9ab4d564a909f0f4660882a0d09a6
RRibeiro8 <rfribeiro@ua.pt>
2017-01-18 16:43:26 +0000

    ...

--- MCDemo/SecondViewController.h
+++ MCDemo/SecondViewController.h
@@ -5,6 +5,7 @@
 //  Created by RRibeiro on 13/01/17.
 //  Copyright © 2017 RRibeiro. All rights reserved.
 //
+
 #import <UIKit/UIKit.h>
 
 @interface SecondViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, UIActionSheetDelegate>
